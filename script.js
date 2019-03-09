8d7527e67271798b2dde462bdbcc4f067373cbd5
fahadmech <47082585+fahadmech@users.noreply.github.com>
2019-03-09 19:27:13 +0530

    IIFE

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ ganguly.Average();*/
 //object.create method
 
 //first we have to create the prototype as simple object
-var personProto = {
+/*var personProto = {
   calculateAge: function() {
     console.log(2016 - this.yearOfBirth);
   }
@@ -111,4 +111,97 @@ function change(a, b) {
 }
 
 change(age, obj);
-console.log(age, obj.city);
+console.log(age, obj.city);*/
+
+//Passing Functions as arguments
+
+/*var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrResult = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrResult.push(fn(arr[i]));
+  }
+  return arrResult;
+}
+
+//functionto calculate ages
+function calcAge(el) {
+  return 2016 - el;
+}
+
+//function to know is the person is of full age
+function isFullAge(el) {
+  return el >= 18;
+}
+
+//function to calculate max heart rate while workout, applies only for adults
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - 0.67 * el);
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calcAge);
+console.log(ages);
+
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(fullAges);
+
+var heratRates = arrayCalc(ages, maxHeartRate);
+console.log(heratRates);*/
+
+//functions returning functions
+/*function interviewQuestion(job) {
+  if (job === "designer") {
+    return function(name) {
+      console.log(name + ", can you expalin what UX design is?");
+    };
+  } else if (job === "teacher") {
+    return function(name) {
+      console.log(name + ", which subject do you teach?");
+    };
+  } else {
+    return function(name) {
+      console.log("Hello " + name + " , what do you do");
+    };
+  }
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+teacherQuestion("john");
+
+var designerQuestion = interviewQuestion("designer");
+designerQuestion("Azaan");
+
+// we can create bunch of it
+designerQuestion("jayesh");
+designerQuestion("sagar");
+
+// Another way of doing it without assigning function in variable;
+interviewQuestion("teacher")("monu");*/
+
+// IIFE Immediately Invoked Function Expression.
+// want to build game but hide the score (Refer the Notes)
+// Normal way of doing it :-
+function game() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+}
+game();
+
+// Now the IIFE way:-
+(function() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+})();
+
+//console.log(score); // Error because score is not defined in global scope.
+
+// Passing Argument in IIFE
+(function(goodluck) {
+  var score = Math.random() * 10;
+  console.log(score >= 5 - goodluck);
+})(5); //always true because score will always be greater than zero.
