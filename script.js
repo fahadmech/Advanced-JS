efcb9ed2363214f9d7813196caf79a4f735619d1
fahadmech <47082585+fahadmech@users.noreply.github.com>
2019-03-10 23:00:08 +0530

    closures

--- script.js
+++ script.js
@@ -205,3 +205,48 @@ game();
   var score = Math.random() * 10;
   console.log(score >= 5 - goodluck);
 })(5); //always true because score will always be greater than zero.
+
+//Closures
+
+function interviewQuestion(job) {
+  var a = "can you expalin what UX design is?";
+  var b = "which subject do you teach?";
+  var c = "what do you do?";
+  return function(name) {
+    if (job === "designer") {
+      console.log(name + ", " + a);
+    } else if (job === "teacher") {
+      console.log(name + ", " + b);
+    } else {
+      console.log("Hello " + name + ", " + c);
+    }
+  };
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+teacherQuestion("Jonas");
+
+var designerQuestion = interviewQuestion("designer");
+designerQuestion("Azaan");
+designerQuestion("Sagar");
+
+interviewQuestion("engineer")("Irfan");
+
+//example
+function retirement(retirementAge) {
+  var a = " Years left until retirement.";
+  return function(yearOfBirth) {
+    var age = 2016 - yearOfBirth;
+    console.log(retirementAge - age + a);
+  };
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+// retirement(66)(1990);
